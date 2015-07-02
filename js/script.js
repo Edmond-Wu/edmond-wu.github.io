725a7a88d68391f5b06d1bad724faedbae422e7e
The-WUUSTER <edma_wu@yahoo.com>
2015-07-02 05:19:53 -0400

    contact info and misc.
    added linkedin and some other minor changes

--- js/script.js
+++ js/script.js
@@ -3,5 +3,7 @@ $(document).ready(function() {
 	$('#hello').fadeTo(1000, 1);
 	$('#monkey').hide();
 	$('#monkey').delay(500).fadeTo(1000, 1);
+	$('#linkedin').hide();
+	$('#linkedin').fadeTo(1000, 1);
 });
 
