ad86e8cd5aec76b825764269ead8d8f8b0970f1c
The-WUUSTER <edma_wu@yahoo.com>
2015-07-03 03:28:50 -0400

    More optimizations

--- js/script.js
+++ js/script.js
@@ -1,27 +1,13 @@
 $(document).ready(function() {
     $('#title').hide();
-	$('#title').fadeTo(1000, 1);
-	$('#monkey').hide();
-	$('#monkey').delay(500).fadeTo(1000, 1);
-	$('#linkedin').hide();
-	$('#linkedin').delay(1000).fadeTo(1000, 1);
-	$('#email').hide();
-	$('#email').delay(500).fadeTo(1000, 1);
-	$('#profpic').hide();
-	$('#profpic').delay(500).fadeTo(1000, 1);
-	$('#p1').hide();
-	$('#p1').delay(1000).fadeTo(1000, 1);
+	$('#delay1').hide();
+	$('#delay2').hide();
+	$('#delay3').hide();
 	$('#p2').hide();
-	$('#p2').delay(1500).fadeTo(1000, 1);
-	$('#cs170').hide();
-	$('#cs170').delay(500).fadeTo(1000, 1);
-	$('#info').hide();
-	$('#info').delay(1000).fadeTo(1000, 1);
 	$('#moreinfo').hide();
-	$('#moreinfo').delay(1500).fadeTo(1000, 1);
-	$('#projects').hide();
-	$('#projects').delay(500).fadeTo(1000, 1);
-	$('#projects2').hide();
-	$('#projects2').delay(1000).fadeTo(1000, 1);
+	$('#title').fadeTo(1000, 1);
+	$('#delay1').delay(500).fadeTo(1000, 1);
+	$('#delay2').delay(1000).fadeTo(1000, 1);	
+	$('#delay3').delay(1500).fadeTo(1000, 1);
 });
 
