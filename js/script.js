ebae32ac4553d73ab12f91e32f986024d7144bf0
The-WUUSTER <edma_wu@yahoo.com>
2015-07-03 03:30:48 -0400

    More edits
    Deleted some things that weren't deleted with previous edits

--- js/script.js
+++ js/script.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
 	$('#delay1').hide();
 	$('#delay2').hide();
 	$('#delay3').hide();
-	$('#p2').hide();
-	$('#moreinfo').hide();
 	$('#title').fadeTo(1000, 1);
 	$('#delay1').delay(500).fadeTo(1000, 1);
 	$('#delay2').delay(1000).fadeTo(1000, 1);	
