fdc02727655e4ceba8ebd80b1d2f724f3d6f2c0f
The-WUUSTER <edma_wu@yahoo.com>
2015-07-03 00:01:18 -0400

    Fancy Titles

--- js/script.js
+++ js/script.js
@@ -1,10 +1,8 @@
 $(document).ready(function() {
-    $('#hello').hide();
-	$('#hello').fadeTo(1000, 1);
+    $('#title').hide();
+	$('#title').fadeTo(1000, 1);
 	$('#monkey').hide();
 	$('#monkey').delay(500).fadeTo(1000, 1);
-	$('h2').hide();
-	$('h2').fadeTo(1000, 1);
 	$('#linkedin').hide();
 	$('#linkedin').delay(1000).fadeTo(1000, 1);
 	$('#email').hide();
