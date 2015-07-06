5b3b8c762871cea151a7ade69b9792ef4019d789
The-WUUSTER <edma_wu@yahoo.com>
2015-07-06 05:04:57 -0400

    Edits
    minor edits to site content

--- js/script.js
+++ js/script.js
@@ -3,9 +3,11 @@ $(document).ready(function() {
 	$('#delay1').hide();
 	$('#delay2').hide();
 	$('#delay3').hide();
+	$('#delay4').hide();
 	$('#title').fadeTo(1000, 1);
 	$('#delay1').delay(500).fadeTo(1000, 1);
 	$('#delay2').delay(1000).fadeTo(1000, 1);	
 	$('#delay3').delay(1500).fadeTo(1000, 1);
+	$('#delay4').delay(2000).fadeTo(1000, 1);
 });
 
