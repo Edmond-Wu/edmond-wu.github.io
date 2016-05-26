1c86c1408733c16af8fd4a8cb9bd723b9f7bd8b4
Edmond Wu <edma_wu@yahoo.com>
2016-05-25 22:51:59 -0400

    Clean up
    Deleted unused slick files, changed id's to classes, and updated 170
    page

--- js/script.js
+++ js/script.js
@@ -4,15 +4,15 @@ $(document).ready(function() {
 		slidesToShow: 3,
 		slidesToScroll: 3
 	});*/
-    $('#title').hide();
-	$('#delay1').hide();
-	$('#delay2').hide();
-	$('#delay3').hide();
-	$('#delay4').hide();
-	$('#title').fadeTo(1000, 1);
-	$('#delay1').delay(750).fadeTo(1000, 1);
-	$('#delay2').delay(1250).fadeTo(1000, 1);	
-	$('#delay3').delay(2000).fadeTo(1000, 1);
-	$('#delay4').delay(2500).fadeTo(1000, 1);
+    $('.title').hide();
+	$('.delay1').hide();
+	$('.delay2').hide();
+	$('.delay3').hide();
+	$('.delay4').hide();
+	$('.title').fadeTo(1000, 1);
+	$('.delay1').delay(750).fadeTo(1000, 1);
+	$('.delay2').delay(1250).fadeTo(1000, 1);	
+	$('.delay3').delay(2000).fadeTo(1000, 1);
+	$('.delay4').delay(2500).fadeTo(1000, 1);
 });
 
