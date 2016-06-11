efceaadb0389a7fc5d2ba95ba12a75cd3e78a246
Edmond Wu <edma_wu@yahoo.com>
2016-06-11 10:48:45 -0400

    More refactoring

--- js/script.js
+++ js/script.js
@@ -1,20 +1,13 @@
 $(document).ready(function() {
-	/*$('.aboutimages').slick({
-		infinite: true,
-		slidesToShow: 3,
-		slidesToScroll: 3
-	});*/
 	$('.toggled').hide();
     $('.title').hide();
 	$('.delay1').hide();
 	$('.delay2').hide();
 	$('.delay3').hide();
-	$('.delay4').hide();
 	$('.title').fadeTo(1000, 1);
 	$('.delay1').delay(750).fadeTo(1000, 1);
 	$('.delay2').delay(1250).fadeTo(1000, 1);	
 	$('.delay3').delay(2000).fadeTo(1000, 1);
-	$('.delay4').delay(2500).fadeTo(1000, 1);
 
 	$('.toggle').click(function() {
 		$('.toggled').slideToggle();
