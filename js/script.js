867ea0e6d78acefbe7f192133842610467f3f3cd
Edmond Wu <edma_wu@yahoo.com>
2016-06-16 10:21:07 -0400

    changed elements to keep space while hidden

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
 	$('.toggled').hide();
-    $('.title').hide();
-	$('.delay1').hide();
-	$('.delay2').hide();
-	$('.delay3').hide();
-	$('.title').fadeTo(1000, 1);
-	$('.delay1').delay(750).fadeTo(1000, 1);
-	$('.delay2').delay(1250).fadeTo(1000, 1);	
-	$('.delay3').delay(2000).fadeTo(1000, 1);
+    $('.title').css("visibility", "hidden");
+	$('.delay1').css("visibility", "hidden");
+	$('.delay2').css("visibility", "hidden");
+	$('.delay3').css("visibility", "hidden");
+	$('.title').css("visibility", "visible").hide().fadeTo(1000, 1);
+	$('.delay1').css("visibility", "visible").hide().delay(750).fadeTo(1000, 1);
+	$('.delay2').css("visibility", "visible").hide().delay(1250).fadeTo(1000, 1);
+	$('.delay3').css("visibility", "visible").hide().delay(2000).fadeTo(1000, 1);
 
 	$('.toggle').click(function() {
 		$('.toggled').slideToggle();
