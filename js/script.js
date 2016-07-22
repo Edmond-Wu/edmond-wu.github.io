7ddd4c254c2e31d1f687609233e5dce1a2dc7031
Edmond Wu <edma_wu@yahoo.com>
2016-07-22 14:42:29 -0400

    minified jquery.js

--- js/script.js
+++ js/script.js
@@ -7,7 +7,6 @@ $(document).ready(function () {
     $('.delay1').css("visibility", "visible").hide().delay(750).fadeTo(1000, 1);
     $('.delay2').css("visibility", "visible").hide().delay(1250).fadeTo(1000, 1);
     $('.delay3').css("visibility", "visible").hide().delay(2000).fadeTo(1000, 1);
-
     $('.toggle').click(function () {
         $('.toggled').slideToggle();
     });
