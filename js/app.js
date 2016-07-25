5719a03588825c9e6309439ecc53eb905fdc6aa1
Edmond Wu <edma_wu@yahoo.com>
2016-07-25 11:28:44 -0400

    moved title tags to the top, about page changes, moved github link from projects to contact

---
+++ js/app.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('.toggled').hide();
+    $('.academics-toggled').hide();
+    $('.work-toggled').hide();
+    $('.title, .delay1, .delay2, .delay3').css("visibility", "hidden");
+    $('.title').css("visibility", "visible").hide().fadeTo(1000, 1);
+    $('.delay1').css("visibility", "visible").hide().delay(750).fadeTo(1000, 1);
+    $('.delay2').css("visibility", "visible").hide().delay(1250).fadeTo(1000, 1);
+    $('.delay3').css("visibility", "visible").hide().delay(2000).fadeTo(1000, 1);
+    $('.toggle').click(function () {
+        $('.toggled').slideToggle();
+    });
+    $('.toggle-academics').click(function () {
+        $('.academics-toggled').slideToggle();
+    });
+    $('.toggle-work').click(function () {
+        $('.work-toggled').slideToggle();
+    });
+});
+
