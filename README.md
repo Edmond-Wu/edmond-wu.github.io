d408ae835ea08ef88527858b1c60245695a92ca6
Edmond Wu <edma_wu@yahoo.com>
2016-07-29 22:09:05 -0400

    switched to angularjs

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # the-wuuster.github.io
-Personal/professional website containing basic personal information, contact information, projects, resume, and resources for the course I TA for.
+Personal/professional website containing basic personal information, contact information, projects, and resources for the course I TA for.
 
 ## Technologies used
-HTML 5 webpages styled with Foundation CSS and animated using jQuery
+AngularJS application styled with Foundation 6 CSS to serve static HTML 5 template pages. Animatated with Angular-Animate and a slider directive.
--- js/app.js
+++ js/app.js
@@ -1,16 +1,87 @@
-$(document).ready(function () {
-    $('.title').css("visibility", "visible").hide().fadeTo(1000, 1);
-    $('.delay1').css("visibility", "visible").hide().delay(750).fadeTo(1000, 1);
-    $('.delay2').css("visibility", "visible").hide().delay(1250).fadeTo(1000, 1);
-    $('.delay3').css("visibility", "visible").hide().delay(2000).fadeTo(1000, 1);
-    $('.toggle').click(function () {
-        $('.toggled').slideToggle();
-    });
-    $('.toggle-academics').click(function () {
-        $('.academics-toggled').slideToggle();
-    });
-    $('.toggle-work').click(function () {
-        $('.work-toggled').slideToggle();
-    });
+'use strict';
+
+var app = angular.module("myApp", ['ngRoute', 'ngAnimate']);
+
+app.config(function($routeProvider) {
+    $routeProvider
+        .when("/main", {
+            templateUrl: "views/main.html",
+            controller: "mainCtrl"
+        })
+        .when('/about', {
+            templateUrl: 'views/about.html',
+            controller: 'aboutCtrl'
+        })
+        .when('/projects', {
+            templateUrl: 'views/projects.html',
+            controller: 'projectsCtrl'
+        })
+        .when('/cs170', {
+            templateUrl: 'views/cs170.html',
+            controller: 'cs170Ctrl'
+        })
+        .when('/contact', {
+            templateUrl: 'views/contact.html',
+            controller: 'contactCtrl'
+        })
+        .otherwise('/main');
 });
 
+app.controller("mainCtrl", function($scope) {});
+
+app.controller("aboutCtrl", function($scope) {});
+
+app.controller("projectsCtrl", function($scope) {});
+
+app.controller("cs170Ctrl", function($scope) {});
+
+app.controller("contactCtrl", function($scope) {});
+
+
+/**
+ * Directive for slide toggle functionality
+ */
+app.directive('sliderToggle', function() {
+    return {
+        restrict: 'AE',
+        link: function(scope, element, attrs) {
+            var target = element.parent()[0].querySelector('[slider]');
+            attrs.expanded = false;
+            element.bind('click', function() {
+                var content = target.querySelector('.slideable_content');
+                if(!attrs.expanded) {
+                    content.style.border = '1px solid rgba(0,0,0,0)';
+                    var y = content.clientHeight;
+                    content.style.border = 0;
+                    target.style.height = y + 'px';
+                } else {
+                    target.style.height = '0px';
+                }
+                attrs.expanded = !attrs.expanded;
+            });
+        }
+    }
+});
+app.directive('slider', function () {
+    return {
+        restrict:'A',
+        compile: function (element, attr) {
+            // wrap tag
+            var contents = element.html();
+            element.html('<div class="slideable_content" style="margin:0 !important; padding:0 !important" >' + contents + '</div>');
+
+            return function postLink(scope, element, attrs) {
+                // default properties
+                attrs.duration = (!attrs.duration) ? '.6s' : attrs.duration;
+                attrs.easing = (!attrs.easing) ? 'ease-in-out' : attrs.easing;
+                element.css({
+                    'overflow': 'hidden',
+                    'height': '0px',
+                    'transitionProperty': 'height',
+                    'transitionDuration': attrs.duration,
+                    'transitionTimingFunction': attrs.easing
+                });
+            };
+        }
+    };
+});
