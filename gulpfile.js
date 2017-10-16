43aef664088914ad9ebacf76ded35d9f1a0d89b0
Edmond Wu <edma_wu@yahoo.com>
2017-10-16 18:05:16 -0400

    used series to build js in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -9,13 +9,13 @@ gulp.task('pack-js', function() {
   return gulp.src(['bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js', 'bower_components/angular/angular.min.js',
     'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-animate/angular-animate.min.js',
     'js/vendor/jquery.min.js', 'bower_components/materialize/dist/js/materialize.min.js'])
-    .pipe(concat('bundle.js'))
+    .pipe(concat('vendor.js'))
     .pipe(minify())
     .pipe(gulp.dest('js'));
 });
 
 gulp.task('add-app-js', function() {
-  return gulp.src(['js/bundle.min.js', 'js/app.js'])
+  return gulp.src(['js/vendor-min.js', 'js/app.js'])
     .pipe(concat('build.js'))
     .pipe(gulp.dest('js'));
 })
@@ -23,9 +23,22 @@ gulp.task('add-app-js', function() {
 gulp.task('pack-css', function() {
   return gulp.src(['bower_components/html5-boilerplate/dist/css/normalize.min.css', 'bower_components/html5-boilerplate/dist/css/main.min.css',
     'bower_components/materialize/dist/css/materialize.min.css', 'bower_components/mdi/css/materialdesignicons.min.css', 'css/app.css'])
-    .pipe(concat('bundle.css'))
+    .pipe(concat('build.css'))
     .pipe(cleanCss())
     .pipe(gulp.dest('css'));
 });
 
-gulp.task('default', gulp.series('pack-js, add-app-js', 'pack-css'));
+gulp.task('default', function() {
+  return runSequence(
+    ['pack-js', 'add-app-js'],
+    'pack-css'
+  );
+});
+
+gulp.task(
+  'default',
+  gulp.parallel(
+    gulp.series('pack-js', 'add-app-js'),
+    'pack-css'
+  )
+);
