5edde9170c332cce3e92a32ce65deda03bf8650a
Edmond Wu <edma_wu@yahoo.com>
2017-10-15 23:42:47 -0400

    configured gulp to automatically minify css/js files

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,20 @@
 var gulp = require('gulp');
-var sync = require('gulp-concat');
+var cleanCss = require('gulp-clean-css');
+var minify = require('gulp-minify');
+var rename = require('gulp-rename');
 
-gulp.task('pack-js', function() {
-  return gulp.src(['js/vendor/*.js', 'js/*.js'])
-    .pipe(concat('bundle.js'))
-    .pipe(gulp.dest('build/js'));
+gulp.task('clean-css', function() {
+  return gulp.src(['css/app.css'])
+    .pipe(cleanCss())
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('css'));
 });
 
-gulp.task('pack-css', function() {
-  return gulp.src(['bower_components/html5-boilerplate/dist/css/*.min.css',
-  'bower_components/materialize/dist/css/materialize.min.css', 'bower_components/mdi/css/materialdesignicons.min.css','css/app.css'])
-    .pipe(concat('stylesheet.css'))
-    .pipe(gulp.dest('build/css'));
+gulp.task('minify-js', function() {
+  return gulp.src(['js/app.js'])
+    .pipe(minify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['pack-js', 'pack-css']);
-
-/*
-<link rel="stylesheet" href="bower_components/html5-boilerplate/dist/css/normalize.min.css">
-<link rel="stylesheet" href="bower_components/html5-boilerplate/dist/css/main.min.css">
-<link rel="stylesheet" href="bower_components/materialize/dist/css/materialize.min.css">
-<link rel="stylesheet" href="bower_components/mdi/css/materialdesignicons.min.css">
-<link rel="stylesheet" href="css/app.min.css" />
- */
+gulp.task('default', ['clean-css', 'minify-js']);
