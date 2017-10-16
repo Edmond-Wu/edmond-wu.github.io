b9bd08158e45682c12f754a57996e2ad26dc553a
Edmond Wu <edma_wu@yahoo.com>
2017-10-16 00:06:35 -0400

    got concat working somewhat

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,7 @@ var gulp = require('gulp');
 var cleanCss = require('gulp-clean-css');
 var minify = require('gulp-minify');
 var rename = require('gulp-rename');
-
-gulp.task('clean-css', function() {
-  return gulp.src(['css/app.css'])
-    .pipe(cleanCss())
-    .pipe(rename('app.min.css'))
-    .pipe(gulp.dest('css'));
-});
+var concat = require('gulp-concat');
 
 gulp.task('minify-js', function() {
   return gulp.src(['js/app.js'])
@@ -17,4 +11,20 @@ gulp.task('minify-js', function() {
     .pipe(gulp.dest('js'));
 });
 
-gulp.task('default', ['clean-css', 'minify-js']);
+gulp.task('pack-js', function() {
+  return gulp.src(['bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js', 'bower_components/angular/angular.min.js',
+    'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-animate/angular-animate.min.js',
+    'js/vendor/jquery.min.js', 'bower_components/materialize/dist/js/materialize.min.js', 'js/app.min.js'])
+    .pipe(concat('bundle.js'))
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('pack-css', function() {
+  return gulp.src(['bower_components/html5-boilerplate/dist/css/normalize.min.css', 'bower_components/html5-boilerplate/dist/css/main.min.css',
+    'bower_components/materialize/dist/css/materialize.min.css', 'css/app.css'])
+    .pipe(concat('bundle.css'))
+    .pipe(cleanCss())
+    .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['minify-js', 'pack-js', 'pack-css']);
