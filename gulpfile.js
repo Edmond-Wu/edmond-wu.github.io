097ecbb73ddb958f7f4be349463cc0d85dfcc312
Edmond Wud <edmond.wu0895@gmail.com>
2017-10-17 12:58:16 -0400

    added some comments to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var rename = require('gulp-rename');
 var concat = require('gulp-concat');
 var series = require('gulp-series');
 
+/* packs vendor js files into a single file and minifies it */
 gulp.task('pack-js', function() {
   return gulp.src(['bower_components/html5-boilerplate/dist/js/vendor/modernizr-2.8.3.min.js', 'bower_components/angular/angular.min.js',
     'bower_components/angular-route/angular-route.min.js', 'bower_components/angular-animate/angular-animate.min.js',
@@ -14,12 +15,14 @@ gulp.task('pack-js', function() {
     .pipe(gulp.dest('js'));
 });
 
+/* adds on app.js separately */
 gulp.task('add-app-js', function() {
   return gulp.src(['js/vendor-min.js', 'js/app.js'])
     .pipe(concat('build.js'))
     .pipe(gulp.dest('js'));
 })
 
+/* packs css files into a single file and then minifies it */
 gulp.task('pack-css', function() {
   return gulp.src(['bower_components/html5-boilerplate/dist/css/normalize.min.css', 'bower_components/html5-boilerplate/dist/css/main.min.css',
     'bower_components/materialize/dist/css/materialize.min.css', 'bower_components/mdi/css/materialdesignicons.min.css', 'css/app.css'])
@@ -35,6 +38,7 @@ gulp.task('default', function() {
   );
 });
 
+/* js stuff needs to be done sequentially */
 gulp.task(
   'default',
   gulp.parallel(
