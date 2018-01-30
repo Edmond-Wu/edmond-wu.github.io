a23214261bacb00b0c5c2de8101ec84f36b9cc4e
Edmond Wud <edmond.wu0895@gmail.com>
2018-01-30 13:18:34 -0500

    moved projects from html to angular

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var app = angular.module("myApp", ['ngRoute', 'ngAnimate']);
+var app = angular.module("myApp", ['ngRoute', 'ngAnimate', 'ngSanitize']);
 app.config(function($routeProvider) {
   $routeProvider
     .when('/about', {
@@ -44,6 +44,36 @@ app.controller("projectsCtrl", function($scope) {
     {
       title: '<a href="https://github.com/Edmond-Wu/socket-chat" target="_blank" class="waves-effect"><b><i>Socket Chat:</i></b></a>',
       description: 'Another chat application, built using socket.io! Users can log in, view other logged-on users, and change their message colors. Front-end designed with AngularJS and Materialize CSS.'
+    },
+    {
+      title: '<a href="https://rutgerstc.github.io" target="_blank" class="waves-effect"><b><i>Rutgers Tzu-Ching:</i></b></a>',
+      description: 'Website for the Rutgers chapter of the Tzu-Chi Collegiate Association. Single-page application designed with AngularJS.'
+    },
+    {
+      title: '<a href="http://devpost.com/software/insightweet" target="_blank" class="waves-effect"><b><i>Insightweets:</i></b></a>',
+      description: 'HackNY 2015 group web hack using IBM\'s Bluemix algorithm to find out what people are like based on their tweets (won best IBM hack at HackNY Spring 2015!)'
+    }
+  ];
+  $scope.otherProjs = [
+    {
+      title: '<a href="../assets/unity/td.html" target="_blank" class="waves-effect"><b><i>Tower Defense:</i></b></a>',
+      description: 'A basic tower defense game with a geometric/cube theme with some dank memes. Made using Unity 5 with C#; requires <a href="http://unity3d.com/webplayer/" target="_blank" class="waves-effect">the Unity3D plug-in</a> to work.'
+    },
+    {
+      title: '<a href="../assets/unity/interstellar_shooter.html" target="_blank" class="waves-effect"><b><i>Interstellar Shooter:</i></b></a>',
+      description: 'Game resembling a classic space-shooter arcade. Made using Unity 5 with C# scripts. Also needs the <a href="http://unity3d.com/webplayer/" target="_blank" class="waves-effect">Unity3D plugin</a>.'
+    },
+    {
+      title: '<a href="https://github.com/Edmond-Wu/PhotoAlbum" target="_blank" class="waves-effect"><b><i>Photo Album:</i></b></a>',
+      description: 'JavaFX CRUD application resembling a photo album. Users can add/edit/delete albums and photos, as well as view/like other users\' photos.'
+    },
+    {
+      title: '<a href="https://github.com/Edmond-Wu/Chess" target="_blank" class="waves-effect"><b><i>Chess:</i></b></a>',
+      description: 'Java project that simulates a complete 2-player chess game with all competitive rules in terminal.'
+    },
+    {
+      title: '<a href="https://github.com/Edmond-Wu/Android-Books" target="_blank" class="waves-effect"><b><i>Android Books:</i></b></a>',
+      description: 'Android book database application that allows for users to subscribe to books to receive new chapter updates.'
     }
   ];
 });
